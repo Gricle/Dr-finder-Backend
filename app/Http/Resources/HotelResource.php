b2c8911c23ec48e8476c3457a689a29e677ba0fe
Mehdi Kaviani <mehdikavyani02@gmail.com>
2024-09-12 15:23:26 +0330

    add hotel resource

--- app/Http/Resources/HotelResource.php
+++ app/Http/Resources/HotelResource.php
@@ -14,6 +14,18 @@ class HotelResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'name' => $this->name,
+            'stars' => $this->stars,
+            'description' => $this->description,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'address' => $this->address,
+            // 'created_at' => $this->created_at,
+            // 'updated_at' => $this->updated_at,
+            // 'deleted_at' => $this->deleted_at,
+        ];
     }
 }
