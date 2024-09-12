ccd8f96bf6fa554a339b52c24f569b6260babefc
Mehdi Kaviani <mehdikavyani02@gmail.com>
2024-09-12 15:09:32 +0330

    add hotel routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DoctorController;
+use App\Http\Controllers\Api\HotelController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,4 +16,12 @@ Route::prefix('doctors')->group(function () {
     Route::get('/{doctor}', [DoctorController::class, 'show']);
     Route::put('/{id}', [DoctorController::class, 'update'])->middleware('auth:sanctum');
     Route::delete('/{doctor}', [DoctorController::class, 'destroy'])->middleware('auth:sanctum');
+});
+
+Route::prefix('hotels')->group(function () {
+    Route::get('/', [HotelController::class, 'index']);
+    Route::post('/', [HotelController::class, 'store']);
+    Route::get('/{hotel}', [HotelController::class, 'show']);
+    Route::put('/{id}', [HotelController::class, 'update'])->middleware('auth:sanctum');
+    Route::delete('/{hotel}', [HotelController::class, 'destroy'])->middleware('auth:sanctum');
 });
